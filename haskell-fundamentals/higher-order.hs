2339855dfaafb4a6cabde276597ad6723821f5ac
tarasowski <dimitri+github@tarasowski.de>
2019-04-14 15:14:14 +0200

    higher order function

---
+++ haskell-fundamentals/higher-order.hs
@@ -0,0 +1,6 @@
+-- In Java you use function to package and reuse your code
+-- In Haskell functions are much more fundamental
+-- Functions in Haskell are like any other type of value like: Int, String
+-- Higher order functions can take and return other functions
+
+
